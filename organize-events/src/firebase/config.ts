580bbebeb668bbc7ed063d9af16636d2382714a6
Минаев Александр <tenshisasha@gmail.com>
2025-06-07 00:23:15 +0300

    правки конфигов firebase

--- organize-events/src/firebase/config.ts
+++ organize-events/src/firebase/config.ts
@@ -3,14 +3,15 @@ import { getDatabase } from "firebase/database";
 
 // Firebase configuration
 const firebaseConfig: FirebaseOptions = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAK_TOb75b9U0VDx93GLq1VkpCnzZm-DE4",
+  authDomain: "participants-seva.firebaseapp.com",
+  projectId: "participants-seva",
+  databaseURL:
+    "https://participants-seva-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "participants-seva.appspot.com",
+  messagingSenderId: "587560809720",
+  appId: "1:587560809720:web:87dd4167777a3d799b770d",
+  measurementId: "G-MLLV2HH2NH",
 };
 
 // Initialize Firebase
